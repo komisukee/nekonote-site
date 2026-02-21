30812c5b182ace6caa3bf512f61b46e75e62e26c
komisukee <takafumi.komiyama.35@gmail.com>
2026-02-21 21:43:59 +0900

    fix: レイアウト修正

--- app/company/page.tsx
+++ app/company/page.tsx
@@ -113,7 +113,7 @@ export default function CompanyPage() {
       <div id="contact" className="bg-[rgb(71,158,187,0.6)] py-10">
         <div className="container">
           <div className="flex flex-col items-center gap-6 py-10">
-            <h3 className="font-bold text-white text-[32px] tracking-wider flex flex-col items-center gap-2">
+            <h3 className="font-bold text-white md:text-[32px] text-[20px] tracking-wider flex flex-col items-center gap-2">
               <span className="text-sm">CONTACT</span>
               <span className="bg-[linear-gradient(transparent_80%,#ffd803_80%)]">
                 お問い合わせ
