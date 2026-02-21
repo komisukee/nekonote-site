cb3c777974186d3e7c9497dc41f943361604fa58
komisukee <takafumi.komiyama.35@gmail.com>
2026-02-22 00:26:27 +0900

    fix: imagesの最適化を無効化

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    images: {
+        unoptimized: true,
+    },
     reactStrictMode: true,
         images: {
         domains: ["images.microcms-assets.io"],
