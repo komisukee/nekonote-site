6c9b5e877538db1699b16759e809dbad59d1beb7
komisukee <takafumi.komiyama.35@gmail.com>
2026-02-22 07:23:55 +0900

    fix: nextConfig修正

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    reactStrictMode: true,
     images: {
         unoptimized: true,
-    },
-    reactStrictMode: true,
-        images: {
         domains: ["images.microcms-assets.io"],
     },
 }
